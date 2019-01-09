6ea052da41bccdec6aa41798d2536e4fd3c8bee3
aleks2414 <alejandroromo14@gmail.com>
2019-01-08 22:44:34 -0600

    dashboar

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,11 @@
 class WelcomeController < ApplicationController
 	before_action :authenticate_user!
   def index
+  	@services1 = Service.where(etapa: 1).order('id ASC')
+  	@services2 = Service.where(etapa: 2).order('id ASC')
+  	@services3 = Service.where(etapa: 3).order('id ASC')
+  	@services4 = Service.where(etapa: 4).order('id ASC')
+  	@services5 = Service.where(etapa: 5).order('id ASC')
+  	@services6 = Service.where(etapa: 6).order('id ASC')
   end
 end
