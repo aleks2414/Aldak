de87d87c54925ed6de5fedb5d78a12781c86b06b
Artem Kulakov <artem.kulakov@gmail.com>
2020-05-22 20:54:09 +0300

    Status enum

--- app/models/charter.rb
+++ app/models/charter.rb
@@ -2,4 +2,6 @@ class Charter < ApplicationRecord
   belongs_to :user
   has_many :services
   has_many :charter_payments
+
+  enum status: %w( debo debe )
 end
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -2,4 +2,6 @@ class Provider < ApplicationRecord
   belongs_to :user
   has_many :services
   has_many :provider_payments
+
+  enum status: %w( debo debe )
 end
