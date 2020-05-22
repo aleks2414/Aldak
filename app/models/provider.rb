cdd24c02f783a900018dac94db5bc7b727e4250d
Artem Kulakov <artem.kulakov@gmail.com>
2020-05-22 21:30:51 +0300

    Set provider status

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -4,4 +4,10 @@ class Provider < ApplicationRecord
   has_many :provider_payments
 
   enum status: %w( debo debe )
+
+  def set_status
+    n1 = provider_payments.map(&:cantidad).sum
+    n2 = services.map(&:proveedor).sum + services.map(&:iva_proveedor).sum
+    n1 - n2 >= 0 ? 'debe' : 'debo'
+  end
 end
