3eb9f12468d6e2bea57dc598be8c6592e967905a
Artem Kulakov <artem.kulakov@gmail.com>
2020-05-23 20:37:19 +0300

    Add links to years and months to /reporte

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,20 @@
 class PagesController < ApplicationController
 
 def reporte
-	@services = Service.all.order('id ASC')
-	@services = @services.paginate(:page => params[:page], :per_page => 10)
+  @years = Service.all.group_by(&:year).keys.uniq
+
+  @year = params[:year] || Date.current.year
+
+  @months = Array(1..12)
+  if @year.to_s == Date.current.year.to_s
+    @month = params[:month] || Date.current.month
+  else
+    @month = 12
+  end
+
+  date = DateTime.new(@year.to_i, @month.to_i, 15) # just a date in the middle of the month
+
+	@services = Service.where(fecha_de_entrega: date.beginning_of_month..date.end_of_month).order(:id).paginate(:page => params[:page])
 end
 
 end
